a069b88fd2c2dbe6ba21f5b0a25734095b4e2db2
Ekrem BAL <ekrem@pragmacraft.com>
2022-12-07 00:27:10 +0300

    minor fix

--- circuits/test/main.test.ts
+++ circuits/test/main.test.ts
@@ -23,22 +23,6 @@ describe("Test zkp circuit and scripts", function () {
     );
     client = await new ZKPClient().init(wasm, zkey);
   });
-  xit("Should able to prove and verify the zkp", async function () {
-    const message = BigNumber.from(
-      "0xABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00"
-    );
-    const signature = await eddsa.sign(message);
-    expect(await eddsa.verify(message, signature, eddsa.pubKey)).to.eq(true);
-    const proof = await client.prove({
-      M: message.toBigInt(),
-      Ax: eddsa.scalarPubKey[0],
-      Ay: eddsa.scalarPubKey[1],
-      S: signature.S,
-      R8x: eddsa.babyjub.F.toObject(signature.R8[0]),
-      R8y: eddsa.babyjub.F.toObject(signature.R8[1]),
-    });
-    expect(proof).not.to.eq(undefined);
-  });
   xit("Should able to parse note", async function () {
     const parsedNote = await client.parseNote(note);
     expect(parsedNote).not.to.eq(undefined);
