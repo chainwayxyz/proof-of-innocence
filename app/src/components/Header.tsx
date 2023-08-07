a2a13d2952a22d266f35c43503e3d91affec07e1
benbaho <aydinbahadirtuna@gmail.com>
2023-08-07 15:44:17 +0300

    feature: demo page styling

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <header>
-      <h1 className="text-3xl font-bold mb-4 mt-6">Proof of Innocence</h1>
+      <h1 className="text-3xl font-bold mb-8 text-slate-950">
+        Proof of Innocence
+      </h1>
     </header>
   );
 };
--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,13 +1,15 @@
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = ({ children }: {children:any}) => {
+const Layout = ({ children }: { children: any }) => {
   return (
     // Make classname such that content is centered and not full width
     // Also make sure that the footer is always at the bottom of the page
-    <div className='text-center'>
-      <Header />
-      <main className='max-w-lg center mx-auto'>{children}</main>
+    <div className="text-center p-4">
+      <div className="flex flex-col justify-self align-items rounded-2xl px-4 pb-8 pt-6 mt-[40px] md:mt-[160px] bg-white shadow-xl border border-slate-300 max-w-[700px] mx-auto">
+        <Header />
+        <main className="max-w-lg center mx-auto">{children}</main>
+      </div>
       <Footer />
     </div>
   );
